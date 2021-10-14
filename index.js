1a15d2f59afa0df209eb37b69d79bb85f756bf11
webersinfotech <53133512+webersinfotech@users.noreply.github.com>
2021-10-14 17:39:00 +0530

    redis connection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Redis = require("ioredis");
-const redis = new Redis({
-  host: "rapidtax.1laadw.0001.aps1.cache.amazonaws.com",
-  port: 6379,
-  tls: {}
-});
+const redis = new Redis("redis://rapidtax.1laadw.0001.aps1.cache.amazonaws.com:6379");
+
+// {
+//   host: "rapidtax.1laadw.0001.aps1.cache.amazonaws.com",
+//   port: 6379,
+//   tls: {}
+// }
